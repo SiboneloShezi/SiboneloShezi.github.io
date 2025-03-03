4682d3d1cc3d5b2fd2ae6b8309433385b4e5ab53
SiboneloShezi <wizardshezi@gmail.com>
2025-03-03 10:23:30 +0200

    updated navigation functionality

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-function showNavElements() {
-  navSection = `
-    <div class="nav-about-section">
-    <nav>
-        <ul>
-        <li>
-            <a href="index.html"> Home</a>
-        </li>
-        <li>
-            <a href="about_me.html">About</a>
-        </li>
+let elementsPresent = false; // Global variable to persist state
 
-        <li>
-            <a href="skills.html">Skills</a>
-        </li>
-        <li>
-            <a href="projects.html">Projects</a>
-        </li>
-        <li>
-            <a href="education.html">Education</a>
-        </li>
-        <li>
-            <a href="experience.html">Experience</a>
-        </li>
-        <li>
-            <a href="contact.html">Contact</a>
-        </li>
+function showNavElements() {
+  const navContainer = document.querySelector(".js-navElements");
+
+  if (!elementsPresent) {
+    elementsPresent = true; // Update state
+
+    let navSection = `
+    <div class="nav-about-section">
+      <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about_me.html">About</a></li>
+          <li><a href="skills.html">Skills</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li><a href="education.html">Education</a></li>
+          <li><a href="experience.html">Experience</a></li>
+          <li><a href="contact.html">Contact</a></li>
         </ul>
-    </nav>
+      </nav>
     </div>
     `;
-  document.querySelector(".js-navElements").innerHTML = navSection;
+
+    navContainer.innerHTML = navSection;
+  } else {
+    elementsPresent = false; // Reset state
+    navContainer.innerHTML = ""; // Hide navigation
+  }
 }
