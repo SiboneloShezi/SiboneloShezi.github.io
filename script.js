9848ac7863afa369393b8e1fd7a17d61d1d4e018
SiboneloShezi <wizardshezi@gmail.com>
2025-03-05 00:07:57 +0200

    updated project file using bootstrap

--- script.js
+++ script.js
@@ -8,23 +8,28 @@ function showNavElements() {
 
     let navSection = `
     <div class="nav-about-section">
-      <nav>
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="about_me.html">About</a></li>
-          <li><a href="skills.html">Skills</a></li>
-          <li><a href="projects.html">Projects</a></li>
-          <li><a href="education.html">Education</a></li>
-          <li><a href="experience.html">Experience</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-      </nav>
-    </div>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about_me.html">About</a></li>
+        <li><a href="skills.html">Skills</a></li>
+        <li><a href="projects.html">Projects</a></li>
+        <li><a href="education.html">Education</a></li>
+        <li><a href="experience.html">Experience</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
     `;
 
     navContainer.innerHTML = navSection;
+    document.querySelector(".js-button").innerHTML =
+      `<button style= "padding: 8px" > <strong style = "width: 7px">x</strong></button>`;
   } else {
     elementsPresent = false; // Reset state
     navContainer.innerHTML = ""; // Hide navigation
+    document.querySelector(".js-button").innerHTML =
+      ` <button  style="padding: 2px; cursor: pointer ; border: none"> 
+      <img style="width: 40px; " src="ThreeLinesIcon.png"/>
+      </button>`;
   }
 }
